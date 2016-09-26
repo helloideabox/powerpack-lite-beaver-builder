cea5c673d9a8f840f0456338f3dc3fd51efe2da5
Achal Jain <achal@ideaboxcreations.com>
2016-09-26 11:25:22 +0530

    Remove column seprators and shadow

--- includes/column-css.php
+++ includes/column-css.php
@@ -5,9 +5,6 @@ function pp_column_render_css( $extensions ) {
     if ( array_key_exists( 'corners', $extensions['col'] ) || in_array( 'corners', $extensions['col'] ) ) {
         add_filter( 'fl_builder_render_css', 'pp_column_round_corners_css', 10, 3 );
     }
-    if ( array_key_exists( 'shadow', $extensions['col'] ) || in_array( 'shadow', $extensions['col'] ) ) {
-        add_filter( 'fl_builder_render_css', 'pp_column_shadow_css', 10, 3 );
-    }
 }
 
 /** Corners */
@@ -35,45 +32,6 @@ function pp_column_round_corners_css( $css, $nodes, $global_settings ) {
         }
 
     <?php $css .= ob_get_clean();
-    }
-
-    return $css;
-}
-
-/** Shadow */
-function pp_column_shadow_css( $css, $nodes, $global_settings ) {
-
-    foreach ( $nodes['columns'] as $column ) {
-
-        ob_start();
-    ?>
-
-        <?php if ( isset( $column->settings->pp_box_shadow ) ) { ?>
-
-            .fl-node-<?php echo $column->node; ?> .fl-col-content {
-                -webkit-box-shadow: <?php echo $column->settings->pp_box_shadow['vertical']; ?>px <?php echo $column->settings->pp_box_shadow['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow['blur']; ?>px <?php echo $column->settings->pp_box_shadow['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color, $column->settings->pp_box_shadow_opacity / 100); ?>;
-                -moz-box-shadow: <?php echo $column->settings->pp_box_shadow['vertical']; ?>px <?php echo $column->settings->pp_box_shadow['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow['blur']; ?>px <?php echo $column->settings->pp_box_shadow['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color, $column->settings->pp_box_shadow_opacity / 100); ?>;
-                box-shadow: <?php echo $column->settings->pp_box_shadow['vertical']; ?>px <?php echo $column->settings->pp_box_shadow['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow['blur']; ?>px <?php echo $column->settings->pp_box_shadow['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color, $column->settings->pp_box_shadow_opacity / 100); ?>;
-                -webkit-transition: -webkit-box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, -webkit-transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                -moz-transition: -moz-box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, -moz-transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                transition: box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                will-change: box-shadow;
-            }
-
-            <?php if ( 'yes' == $column->settings->pp_box_shadow_hover_switch ) { ?>
-                .fl-node-<?php echo $column->node; ?> .fl-col-content:hover {
-                    -webkit-box-shadow: <?php echo $column->settings->pp_box_shadow_hover['vertical']; ?>px <?php echo $column->settings->pp_box_shadow_hover['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow_hover['blur']; ?>px <?php echo $column->settings->pp_box_shadow_hover['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color_hover, $column->settings->pp_box_shadow_opacity_hover / 100); ?>;
-                    -moz-box-shadow: <?php echo $column->settings->pp_box_shadow_hover['vertical']; ?>px <?php echo $column->settings->pp_box_shadow_hover['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow_hover['blur']; ?>px <?php echo $column->settings->pp_box_shadow_hover['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color_hover, $column->settings->pp_box_shadow_opacity_hover / 100); ?>;
-                    box-shadow: <?php echo $column->settings->pp_box_shadow_hover['vertical']; ?>px <?php echo $column->settings->pp_box_shadow_hover['horizontal']; ?>px <?php echo $column->settings->pp_box_shadow_hover['blur']; ?>px <?php echo $column->settings->pp_box_shadow_hover['spread']; ?>px <?php echo pp_hex2rgba('#'.$column->settings->pp_box_shadow_color_hover, $column->settings->pp_box_shadow_opacity_hover / 100); ?>;
-                    -webkit-transition: -webkit-box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, -webkit-transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                    -moz-transition: -moz-box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, -moz-transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                    transition: box-shadow <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out, transform <?php echo $column->settings->pp_box_shadow_transition; ?>ms ease-in-out;
-                }
-            <?php } ?>
-
-        <?php }
-
-        $css .= ob_get_clean();
     }
 
     return $css;
