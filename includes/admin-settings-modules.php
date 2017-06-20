48576975f5ee1743e2c946273a2248e8e276c0a0
Achal Jain <achal@ideaboxcreations.com>
2017-06-20 16:17:20 +0530

    Added upgrade tab

--- includes/admin-settings-modules.php
+++ includes/admin-settings-modules.php
@@ -11,16 +11,18 @@ $modules = array(
     'content'   => array(
         'name'      => __('Content Modules', 'bb-powerpack'),
         'modules'   => array(
+            array('title' => __('Info Box', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/info-box/', 'installed' => true),
+            array('title' => __('Info List', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/info-list/', 'installed' => true),
+            array('title' => __('Dual Button', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/buttons/', 'installed' => true),
+            array('title' => __('Smart Heading', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/headings/', 'installed' => true),
+            array('title' => __('Dual Color Headings', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/dual-color-heading/', 'installed' => true),
+            array('title' => __('Business Hours', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/business-hours/', 'installed' => true),
+            array('title' => __('Icon List', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/#', 'installed' => true),
             array('title' => __('Content Grid', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/content-grid/'),
             array('title' => __('Content Tiles', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/content-tiles/'),
             array('title' => __('Custom Grid', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
             array('title' => __('Team Member', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/team-member/'),
-            array('title' => __('Info List', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/info-list/'),
             array('title' => __('Smart Button', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/buttons/'),
-            array('title' => __('Dual Button', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/buttons/'),
-            array('title' => __('Smart Heading', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/headings/'),
-            array('title' => __('Dual Color Headings', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/dual-color-heading/'),
-            array('title' => __('Info Box', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/info-box/'),
             array('title' => __('Image', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/images/'),
             array('title' => __('Testimonials', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/testimonial-slider/'),
             array('title' => __('Advanced Tabs', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/advanced-tabs/'),
@@ -29,27 +31,25 @@ $modules = array(
             array('title' => __('Restaurant Menu', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/restaurant-menu/'),
             array('title' => __('Timeline', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/timeline/'),
             array('title' => __('Pricing Table', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/pricing-table/'),
-            array('title' => __('Business Hours', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/business-hours/'),
-            array('title' => __('Icon List', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
             array('title' => __('Social Icons', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
         )
     ),
     'creative'  => array(
         'name'      => __('Creative Modules', 'bb-powerpack'),
         'modules'   => array(
+            array('title' => __('Fancy Heading', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/fancy-heading/', 'installed' => true),
+            array('title' => __('Spacer', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/#', 'installed' => true),
             array('title' => __('Logo Carousel', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/logo-grid-carousel/'),
             array('title' => __('Dot Navigation', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/dot-navigation/'),
             array('title' => __('Highlight Box', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/highlight-box/'),
             array('title' => __('Smart Banner', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/smart-banner/'),
             array('title' => __('Hover Cards', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/hover-cards/'),
             array('title' => __('Flip Box', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/flip-box/'),
-            array('title' => __('Fancy Heading', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/fancy-heading/'),
             array('title' => __('Divider', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/line-separator/'),
             array('title' => __('3D Slider', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
             array('title' => __('Gallery', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
             array('title' => __('Gallery Carousel', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
             array('title' => __('Filterable Gallery', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
-            array('title' => __('Spacer', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/#'),
         )
     ),
     'lead_gen'  => array(
@@ -65,10 +65,10 @@ $modules = array(
     'forms' => array(
         'name'      => __('Form Styler Modules', 'bb-powerpack'),
         'modules'   => array(
+            array('title' => __('Contact Form 7', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/', 'installed' => true),
             array('title' => __('Contact Form', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/forms/'),
             array('title' => __('Gravity Forms', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
             array('title' => __('WPForms', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
-            array('title' => __('Contact Form 7', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
             array('title' => __('Formidable Forms', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
             array('title' => __('Ninja Forms', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
             array('title' => __('Caldera Forms', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/gravity-forms/'),
@@ -77,13 +77,13 @@ $modules = array(
     'extensions'    => array(
         'name'          => __('Extensions', 'bb-powerpack'),
         'modules'       => array(
-            array('title' => __('Row Separators', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/row-separators/'),
+            array('title' => __('Row Separators', 'bb-powerpack'), 'badge' => '3', 'demo' => 'https://wpbeaveraddons.com/demo/row-separators/', 'installed' => true),
+            array('title' => __('Row Gradient', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/row-effects/', 'installed' => true),
+            array('title' => __('Column Round Corners', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/column-extensions/', 'installed' => true),
             array('title' => __('Row Expander', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/row-effects/'),
             array('title' => __('Row Overlay Effects', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/row-effects/'),
-            array('title' => __('Row Gradient', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/row-effects/'),
             array('title' => __('Column Separators', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/column-separator/'),
             array('title' => __('Column Shadow', 'bb-powerpack'), 'badge' => '1', 'demo' => 'https://wpbeaveraddons.com/demo/column-extensions/'),
-            array('title' => __('Round Corners', 'bb-powerpack'), 'badge' => '0', 'demo' => 'https://wpbeaveraddons.com/demo/column-extensions/'),
             array('title' => __('Column Gradient', 'bb-powerpack'), 'badge' => '2', 'demo' => 'https://wpbeaveraddons.com/demo/column-extensions/'),
         )
     )
@@ -98,7 +98,14 @@ $modules = array(
                 <h2 class="pp-modules-category"><?php echo $list['name']; ?> <span class="pp-modules-count"><?php echo count($list['modules']); ?></span></h2>
                 <ul class="pp-modules">
                     <?php foreach ( $list['modules'] as $module ) : ?>
-                        <li class="pp-module pp-badge-<?php echo $module['badge']; ?>"><a href="<?php echo $module['demo']; ?>" target="_blank"><?php echo $module['title']; ?></a></li>
+                        <li class="pp-module pp-badge-<?php echo $module['badge']; ?>">
+                            <a href="<?php echo $module['demo']; ?>" target="_blank">
+                                <?php echo $module['title']; ?>
+                                <?php if ( isset( $module['installed'] ) && $module['installed'] ) { ?>
+                                    <span class="pp-module-status"> - <?php _e('Installed!', 'bb-powerpack'); ?></span>
+                                <?php } ?>
+                            </a>
+                        </li>
                     <?php endforeach; ?>
                 </ul>
             </div>
